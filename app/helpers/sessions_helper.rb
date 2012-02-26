38329bcf2b3debdbbbc3de9b7f6f822a8028dd03
Jordan Conley <jordanconley1@gmail.com>
2012-02-26 12:37:44 -1000

    Change to session

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,13 @@
 module SessionsHelper
   def sign_in(user)
-    cookies.permanent[:remember_token] = user.remember_token
+    session[:user_id] = user.id
+    #cookies.permanent[:remember_token] = user.remember_token
     current_user = user
   end
 
   def sign_out
-    cookies.delete(:remember_token)
+    #cookies.delete(:remember_token)
+    session.delete(:user_id)
   end
 
   def signed_in?
@@ -23,7 +25,8 @@ module SessionsHelper
   private
 
   def user_from_remember_token
-    remember_token = cookies[:remember_token]
-    User.find_by_remember_token(remember_token) unless remember_token.nil?
+    #remember_token = cookies[:remember_token]
+    #User.find_by_remember_token(remember_token) unless remember_token.nil?
+    User.find(session[:user_id]) unless session[:user_id].nil?
   end
 end
